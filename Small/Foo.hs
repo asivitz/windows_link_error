4078a50a3ef6fa1141314f648b7cce8886f7e255
Axis Sivitz <aosivitz@gmail.com>
2025-07-24 16:10:43 -0400

    Maybe we can get rid of the vulkan dependency

--- Small/Foo.hs
+++ Small/Foo.hs
@@ -3,6 +3,10 @@
 module Small.Foo where
 
 import Small.Dep (frag)
-import Vulkan.Utils.ShaderQQ.GLSL.Glslang (compileShaderQ)
+import Language.Haskell.TH
+-- import Vulkan.Utils.ShaderQQ.GLSL.Glslang (compileShaderQ)
 
-compiled = $(compileShaderQ Nothing "frag" Nothing frag)
+-- compiled = $(compileShaderQ Nothing "frag" Nothing frag)
+
+answer :: Int
+answer = $(litE (IntegerL (6 * 7)))  -- evaluates to 42 at compile time
